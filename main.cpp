72fae8f0d8f0b1d1170ea21e4ce132c2d240d3ed
ollie <ollie@ollierobinson.co.uk>
2023-10-22 22:17:32 +0100

    Add GLAD

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <glad/glad.h>
 #include <iostream>
 
 int main(int argc, char* argv[]) {
@@ -7,7 +8,16 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
     SDL_Window* window = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    SDL_GL_MakeCurrent(window, context);
+
+    gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress);
 
     if (window == nullptr) {
         std::cout << "Failed to create window: " << SDL_GetError() << std::endl;
@@ -31,9 +41,14 @@ int main(int argc, char* argv[]) {
             }
         }
 
+        glClearColor(1.0, 0.5, 0.25, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        SDL_GL_SetSwapInterval(1);
         SDL_GL_SwapWindow(window);
     }
 
+    SDL_GL_DeleteContext(context);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
